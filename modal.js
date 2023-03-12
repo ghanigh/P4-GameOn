df9331ec29af5dc0f7f6a2fd461451bbdae8e824
Rhani M <rhani.contact@gmail.com>
2023-03-12 20:03:02 +0100

    true/false

--- modal.js
+++ modal.js
@@ -41,7 +41,6 @@ const quantityText = document.getElementById("quantityText");
 const locationText = document.getElementById("locationText");
 const conditionText = document.getElementById("conditionText");
 
-
 // Fonction pour afficher les messages de validation sur la page Web
 function showMessage(message) {
   const div = document.createElement("div");
@@ -97,7 +96,7 @@ function validateField(field, regEx, message, fieldText) {
     fieldText.innerHTML = "";
     formIsValid = true;
   } else {
-    fieldText.setAttribute("data-error","error");
+    fieldText.setAttribute("data-error", "error");
     fieldText.classList.add("error");
     fieldText.innerHTML = message;
     formIsValid = false;
@@ -123,24 +122,24 @@ quantity.addEventListener("change", function () {
     "Veuillez indiquer un nombre de tournois valide",
     document.getElementById("quantityText")
   );
+  const fieldValue = inputField.value.trim();
+validateField(fieldValue, validateFn);
+
 });
 
 function validateField(input, validateFn, errorMessage, messageElement) {
-  const validClass = "border-success";
-  const invalidClass = "border-danger";
-  const successMessage = "Champ valide";
-  const successClass = "text-success";
-  const failureClass = "text-danger";
-
   if (!input.value) {
-    setMessage(input, messageElement, "Veuillez remplir ce champ", failureClass, invalidClass);
-    return false;
-  } else if (!validateFn(input.value)) {
-    setMessage(input, messageElement, errorMessage, failureClass, invalidClass);
+    setMessage(input, messageElement, "Veuillez remplir ce champ", true);
+    return true;
+  }
+  if (typeof validateFn === "function" && !validateFn(fieldValue))
+  {
+
+    setMessage(input, messageElement, errorMessage, true);
     return false;
   } else {
-    setMessage(input, messageElement, successMessage, successClass, validClass);
-    return true;
+    setMessage(input, messageElement, "champ valide", false);
+    return false;
   }
 }
 
@@ -154,157 +153,156 @@ function isValidDate(value) {
   return !isNaN(Date.parse(value));
 }
 
-function setMessage(input, messageElement, message, messageClass, inputClass, validClass, invalidClass) {
-  if (!input || !messageElement || !message || !messageClass || !inputClass) {
-    throw new Error("Les arguments input, messageElement, message, messageClass et inputClass sont obligatoires.");
-  }
+// Affiche le message d'erreur ou de succes
+function setMessage(input, messageElement, message, success) {
+  const invalidClass = "border-danger";
+  const validClass = "border-success";
+  const successClass = "text-success";
+  const failureClass = "text-danger";
 
   messageElement.innerHTML = message;
-  messageElement.classList.remove(inputClass + "-success", inputClass + "-failure");
-  messageElement.classList.add(messageClass);
+  // Affichage du message de succes
+  if (success) {
+    input.classList.remove(invalidClass);
+    input.classList.add(validClass);
+    messageElement.classList.remove(failureClass);
+    messageElement.classList.add(successClass);
 
-  if (validClass && invalidClass) {
-    input.classList.remove(validClass, invalidClass);
+    return true;
   }
-  input.classList.add(inputClass);
+  // Affichage du message d'erreurs
+  input.classList.remove(validClass);
+  input.classList.add(invalidClass);
+  messageElement.classList.remove(successClass);
+  messageElement.classList.add(failureClass);
+
+  return false;
+}
+
+// ----- NOMBRE DE TOURNOIS -----
+quantity.addEventListener("change", function () {
+  validateQuantity(this);
+});
+
+function validateQuantity(input) {
+  const isValid = isValidQuantity(input.value);
+
+  if (!isValid) {
+    setMessage(input, quantityText, "Champs valide", true);
+    return true
+  } else if (input.value > 50) {
+    setMessage(input, quantityText, "Nous n'avons pas organisé autant de tournois !", false);
+    return false;
+  } else {
+    setMessage(input, quantityText, "Merci d'indiquer le nombre de tournois", false);
+    return false;
+   ; 
+  }
+}
+
+function isValidQuantity(value) {
+  const parsedValue = parseInt(value);
+  return !isNaN(parsedValue) && parsedValue > 0 && parsedValue <= 50;
+}
+
+// ----- VILLES -----
+const locationTournament = document.querySelectorAll('input[type="radio"]');
+locationTournament.forEach((checkedBoxInput) =>
+  checkedBoxInput.addEventListener("change", function () {
+    validateLocationTournament();
+  })
+);
+
+// Validation du nombre de tournois
+function validateLocationTournament() {
+  if (verifLocationTournament()) {
+    return setMessage(locationTournament[0], locationText, true, "Champs valide");
+  }
+  return setMessage(locationTournament[0], locationText, false, "Merci de cocher une ville");
+}
+
+function verifLocationTournament() {
+  for (let i = 0; i < locationTournament.length; i++) {
+    if (locationTournament[i].checked) {
+      return true;
+    }
+  }
+  return false;
 }
 
 
-// ----- NOMBRE DE TOURNOIS -----
-quantity.addEventListener('change', function() {
-  validateQuantity(this);
-  });
-  
-  function validateQuantity(input) {
-  const errorMessage = "Merci d'indiquer le nombre de tournois";
-  const validClass = "border-success";
-  const invalidClass = "border-danger";
-  const successMessage = "Champs valide";
-  const successClass = "text-success";
-  const failureClass = "text-danger";
-  
-  const isValid = isValidQuantity(input.value);
-  
-  if (!isValid) {
-  setMessage(input, quantityText, errorMessage, failureClass, invalidClass);
-  return false;
-  } else if (input.value > 50) {
-  setMessage(input, quantityText, "Nous n'avons pas organisé autant de tournois !", failureClass, invalidClass);
-  return false;
-  } else {
-  setMessage(input, quantityText, successMessage, successClass, validClass);
-  return true;
-  }
-  }
-  
-  function isValidQuantity(value) {
-  const parsedValue = parseInt(value);
-  return !isNaN(parsedValue) && parsedValue > 0 && parsedValue <= 50;
-  }
-  
-  // ----- VILLES -----
-  const locationTournament = document.querySelectorAll('input[type="radio"]');
-  locationTournament.forEach((checkedBoxInput) => checkedBoxInput.addEventListener('change', function() {
-  validateLocationTournament();
-  }));
-  
-  function validateLocationTournament() {
-  const errorMessage = "Merci de cocher une ville";
-  const successMessage = "Champs valide";
-  const successClass = "text-success";
-  const failureClass = "text-danger";
-  const isChecked = verifLocationTournament();
-  
-  if (!isChecked) {
-  setMessage(locationTournament[0], locationText, errorMessage, failureClass);
-  return false;
-  } else {
-  setMessage(locationTournament[0], locationText, successMessage, successClass);
-  return true;
-  }
-  }
-  
-  function verifLocationTournament() {
-  for (let i = 0; i < locationTournament.length; i++) {
-  if (locationTournament[i].checked) {
-  return true;
-  }
-  }
-  return false;
-  }
-  
-  // ----- CONDITIONS -----
-  const condition = document.querySelector("#checkbox1");
-  condition.addEventListener('change', function() {
+// ----- CONDITIONS -----
+const condition = document.querySelector("#checkbox1");
+condition.addEventListener("change", function () {
   validateCondition();
-  });
-  
-  function validateCondition() {
+});
+
+function validateCondition() {
   const errorMessage = "Merci d'accepter les conditions d'utilisations";
   const successMessage = "Champs valide";
   const successClass = "text-success";
   const failureClass = "text-danger";
-  
+
   if (!condition.checked) {
-  setMessage(condition, conditionText, errorMessage, failureClass);
-  return false;
+    setMessage(condition, conditionText, successClass, successMessage);
+    return true;
   } else {
-  setMessage(condition, conditionText, successMessage, successClass);
-  return true;
-  }
+    setMessage(condition, conditionText, failureClass, errorMessage);
+    return false;
   }
   
-  //----- BTN VALIDATION -----
-  function openRemerciments() {
+}
+
+//----- BTN VALIDATION -----
+function openRemerciments() {
   form.style.display = "none";
   validForm.style.display = "flex";
   validMessage.innerHTML = "Merci pour votre inscription";
-  }
-  
-  function validate(event) {
-    event.preventDefault();
-  
-    const regExTypeText = /[\w'-]+/;
-    const regExTypeEmail = /\S+@\S+\.\S+/;
-  
-    const firstName = document.querySelector('#firstname');
-    const lastName = document.querySelector('#lastname');
-    const email = document.querySelector('#email');
-    const birthdate = document.querySelector('#birthdate');
-    const quantity = document.querySelector('#quantity');
-  
-    const firstText = document.querySelector('.firstname.error');
-    const lastText = document.querySelector('.lastname.error');
-    const emailText = document.querySelector('.email.error');
-  
-    if (
-      validateField(firstName, regExTypeText, firstText) &&
-      validateField(lastName, regExTypeText, lastText) &&
-      validateField(email, regExTypeEmail, emailText) &&
-      validateBirthdate(birthdate) &&
-      validateQuantity(quantity) &&
-      validateLocationTournament() &&
-      validateCondition()
-    ) {
-      openRemerciments();
-    } else {
-      alert("Merci de remplir correctement votre inscription");
-    }
-  }
-  
-  
-  // Validation du formulaire
-  const btnSubmit = document.getElementById("btnSubmit");
+}
 
-  form.addEventListener("submit", validate);
-  
-  btnSubmit.addEventListener("click", function(event) {
+function validate(event) {
+  event.preventDefault();
+
+  const regExTypeText = /[\w'-]+/;
+  const regExTypeEmail = /\S+@\S+\.\S+/;
+
+  const firstName = document.querySelector("#firstname");
+  const lastName = document.querySelector("#lastname");
+  const email = document.querySelector("#email");
+  const birthdate = document.querySelector("#birthdate");
+  const quantity = document.querySelector("#quantity");
+
+  const firstText = document.querySelector(".firstname.error");
+  const lastText = document.querySelector(".lastname.error");
+  const emailText = document.querySelector(".email.error");
+
+  if (
+    validateField(firstName, regExTypeText, firstText) &&
+    validateField(lastName, regExTypeText, lastText) &&
+    validateField(email, regExTypeEmail, emailText) &&
+    validateBirthdate(birthdate) &&
+    validateQuantity(quantity) &&
+    validateLocationTournament() &&
+    validateCondition()
+  ) {
+    openRemerciments();
+  } else {
+    alert("Merci de remplir correctement votre inscription");
+  }
+}
+
+// Validation du formulaire
+const btnSubmit = document.getElementById("btnSubmit");
+
+form.addEventListener("submit", validate);
+
+btnSubmit.addEventListener("click", function (event) {
   event.preventDefault(); // Empêche l'envoi par défaut du formulaire
   if (form.checkValidity()) {
-    form.submit(); // Envoyer le formulaire si tout est valide
+    const modalBody = document.getElementsByClassName("modal-body");
+    modalBody.innerHTML = "bravo pour votre inscription !";
   } else {
     // Afficher un message d'erreur ou une indication pour l'utilisateur
     // sur les champs de saisie qui ont besoin d'être corrigés
   }
 });
-
